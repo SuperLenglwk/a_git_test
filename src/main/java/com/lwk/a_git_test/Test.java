bc47a3aadecdefc709a32755fd6ea06a0065534a
SuperLenglwk <38282477+SuperLenglwk@users.noreply.github.com>
2022-03-10 14:31:53 +0800

    同步更新到gitee

--- src/main/java/com/lwk/a_git_test/Test.java
+++ src/main/java/com/lwk/a_git_test/Test.java
@@ -9,5 +9,6 @@ public class Test {
         System.out.println("master test");
         System.out.println("hello 分支1 test");
         System.out.println("push test");
+        System.out.println("gitee test");
     }
 }
